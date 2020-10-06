2595fe15cf2a468a10650015e4d24811539a8030
PhillipHoribe <Horibe.phillip@globo.com>
2020-10-06 17:50:35 -0300

    First Commit

---
+++ XF.LocalDB/XF.LocalDB.Android/SQLite_Android.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XF.LocalDB.Droid;
+using XF.LocalDB.Data;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_Android))]
+namespace XF.LocalDB.Droid
+{
+    public class SQLite_Android : ISQLite
+    {
+        public SQLite_Android()
+        {
+        }
+        public SQLite.SQLiteConnection
+        GetConexao()
+        {
+            var arquivodb = "ifspdb.db3";
+            string caminho =
+            System.Environment.GetFolderPath
+            (System.Environment.SpecialFolder.Personal);
+            var local = Path.Combine(caminho, arquivodb);
+            var conexao = new
+            SQLite.SQLiteConnection(local);
+            return conexao;
+        }
+    }
+}
